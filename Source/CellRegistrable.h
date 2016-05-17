790595202e87b9944ce19b306cac77e652b0d356
Juan Alberto Uribe <juanalberto@kogimobile.com>
2016-05-17 17:27:27 -0500

    Initial commit

---
+++ Source/CellRegistrable.h
@@ -0,0 +1,19 @@
+//
+//  CellRegistrable.h
+//  CellRegistrable
+//
+//  Created by Juan Alberto Uribe on 5/17/16.
+//  Copyright © 2016 Kogi Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CellRegistrable.
+FOUNDATION_EXPORT double CellRegistrableVersionNumber;
+
+//! Project version string for CellRegistrable.
+FOUNDATION_EXPORT const unsigned char CellRegistrableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CellRegistrable/PublicHeader.h>
+
+
